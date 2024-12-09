dc100311b9cdc00f86ea64c5c20dc8c9f4a6a283
Tariqkalam <134319776+Tariqkalam@users.noreply.github.com>
2024-12-10 01:35:39 +0500

    Add files via upload

---
+++ page.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Upperheader, { Header } from '@/components/header'
+import Footer from '@/components/footer'
+
+const Home = () => {
+  return (
+    <div>
+    <Upperheader/>
+    <Header/>
+    <Footer/>    
+
+    </div>
+  )
+}
+
+export default Home
